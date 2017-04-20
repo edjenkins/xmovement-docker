d39c7709045575239dd032aec8dab5b66ae9186c
root <root@xmovement-docker.localdomain>
2017-04-20 20:38:49 +0000

    Updated run queue and caddyfile

--- scripts/run-queue.sh
+++ scripts/run-queue.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 php /var/www/citylit/artisan queue:work database --queue default,emails --sleep 3 --tries 3 --daemon
+php /var/www/create4dementia/artisan queue:work database --queue default,emails --sleep 3 --tries 3 --daemon
