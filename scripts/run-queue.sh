66df969cd6991d5d890c44d11295800614353ec9
root <root@xmovement-docker.localdomain>
2017-04-20 21:03:53 +0000

    Updated run queue

--- scripts/run-queue.sh
+++ scripts/run-queue.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-php /var/www/citylit/artisan queue:work database --queue default,emails --sleep 3 --tries 3 --daemon
-php /var/www/create4dementia/artisan queue:work database --queue default,emails --sleep 3 --tries 3 --daemon
+php /var/www/citylit/artisan queue:work database --queue default,emails --sleep 3 --tries 3
+php /var/www/create4dementia/artisan queue:work database --queue default,emails --sleep 3 --tries 3
+
+echo "Queue ran for citylit, create4dementia";
